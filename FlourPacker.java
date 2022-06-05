642b33f6ab9f175dbb315acd3ad7be21a7705de2
Akilesh <akileshakileshs1234@gmail.com>
2022-06-05 10:54:02 +0530

    Add files via upload

---
+++ FlourPacker.java
@@ -0,0 +1,36 @@
+package com.akilesh;
+
+public class FlourPacker {
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        if ((bigCount < 0) || (smallCount < 0) || (goal < 0)) {
+            return false;
+        }
+
+        if ((bigCount != 0) && (smallCount != 0)) {
+            for (int i = 1; i <= bigCount; i++) {
+                if ((i * 5) == goal) {
+                    return true;
+                }
+                if ((goal - (i * 5)) <= smallCount) {
+                    for (int j = 1; j <= smallCount; j++) {
+                        if ((i * 5) + j == goal) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                goal += i * 5;
+            }
+            if (smallCount >= goal) {
+                return true;
+            }
+        }
+        if ((bigCount == 0) && (smallCount >= goal)) {
+            return true;
+        }
+        if ((smallCount == 0) && (bigCount * 5 == goal)) {
+            return true;
+        }
+        return false;
+    }
+}
