f0d9418de7bc907d70e0ece717ed9e1a54511615
Akilesh <akileshakileshs1234@gmail.com>
2022-06-05 10:55:21 +0530

    Update FlourPacker.java

--- FlourPacker.java
+++ FlourPacker.java
@@ -1,5 +1,3 @@
-package com.akilesh;
-
 public class FlourPacker {
     public static boolean canPack(int bigCount, int smallCount, int goal) {
         if ((bigCount < 0) || (smallCount < 0) || (goal < 0)) {
