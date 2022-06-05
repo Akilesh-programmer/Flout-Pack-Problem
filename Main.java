b821b892a462752cb885eb50af730de3a7864cb6
Akilesh <akileshakileshs1234@gmail.com>
2022-06-05 10:55:18 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
-package com.akilesh;
-
 public class Main {
-
+    
+    // The first parameter is for the big bags, one bag has 5 kg, the second one is for small bag, one bag has 1 kg, we need to tell if we can achieve the 
+    // goal, that is total kgs, with the given small and big bag combination.
     public static void main(String[] args) {
         System.out.println(FlourPacker.canPack(1, 0, 4));
         System.out.println(FlourPacker.canPack(1, 0, 5));
